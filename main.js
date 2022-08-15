6753b2c57ac62b66778ffb0f00460a19eafa1c96
dijon64 <john1.mclean@outlook.com>
2022-08-15 11:07:49 -0400

    load highcharts module

--- main.js
+++ main.js
@@ -17,3 +17,29 @@ map.on('click', function (e) {
     lng = e.latlng.lng;
     console.log(lat, lng);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const chart = Highcharts.chart('container', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+});
