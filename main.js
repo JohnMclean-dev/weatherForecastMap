2c86d49d33e3d0c0c07ad6e54b7e8fc0010a3790
dijon64 <john1.mclean@outlook.com>
2022-08-18 12:48:47 -0400

    understanding async functions

--- main.js
+++ main.js
@@ -50,11 +50,21 @@ function generateId() {
 };
 
 // function to call api
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=0&lon=0&appid=' + key;
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=0&lon=0&appid=' + key;
 
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+async function test(url) {
+    hello = await getWeather(url);
+    console.log(hello)
+};
+
+async function getWeather(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.log(data);
+    return data
+};
+
+test(apiUrl);
 
 // function to handle click event on map
 var ids = [];
